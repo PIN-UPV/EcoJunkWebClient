3474c1bdd28c92b0a9c6187f86061de0dc5616ea
stronquens <stronquens@gmail.com>
2018-10-02 13:09:41 +0200

    Refactoring in boilerplate. Added vue material

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/'
+import store from './store/'
 
 Vue.config.productionTip = false
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+Vue.use(VueMaterial)
 
 new Vue({
   router,
