878655931cf9ea32eed7468dc12bd7f74ba56ac5
Armando Maya <stronquens@gmail.com>
2018-10-07 18:51:01 +0200

    Added leaflet views

---
+++ src/views/Map.vue
@@ -0,0 +1,41 @@
+<style scoped>
+@import "~leaflet/dist/leaflet.css";
+
+.mymap {
+  height: 90vh;
+  widows: 100vw;
+}
+</style>
+
+<template>
+    <div class="mymap">
+        <h1>Mapa Vue Leaflet</h1>
+        <l-map ref="map" :zoom=12 :center="[39.4520498, -0.440134]">
+            <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+            <l-marker :lat-lng="[39.4520498, -0.440134]" />
+        </l-map>
+    </div>
+</template>
+
+<script>
+// https://github.com/KoRiGaN/Vue2Leaflet NO DOCS
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import L from "vue2-leaflet";
+export default {
+  name: "Map",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    L
+  },
+  data() {
+    return {
+      mymap: this.$refs.map.mapObject
+    }
+  },
+  mounted() {
+    this.$refs.map.mapObject._onResize();
+  }
+};
+</script>
---
+++ src/views/Map2.vue
@@ -0,0 +1,39 @@
+<style scoped>
+@import "https://unpkg.com/leaflet@1.3.4/dist/leaflet.css";
+#map {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
+
+<template>
+    <div class="mymap">
+        <h1>Mapa Leaflet</h1>
+        <div id="map"></div>
+    </div>
+</template>
+
+<script>
+import L from "leaflet";
+
+export default {
+  name: "Mapa2",
+  data() {
+    return {};
+  },
+  mounted() {
+    var map = L.map("map").setView([39.4520498, -0.440134], 11);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([39.4697992, -0.3791969])
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      ;
+  }
+};
+</script>
+
