9e437941b3ac4339ca35b400396d6cff3445c7f4
David Flor <davidfb1997@gmail.com>
2018-11-01 19:53:55 +0100

    Validation 1/2

--- src/views/FormularioAcuerdo.vue
+++ src/views/FormularioAcuerdo.vue
@@ -1,47 +1,58 @@
 <template>
-  <div id = "form">
+  <div id = "formulario">
 
-    <md-field>
+    <form novalidate @submit.prevent="validateForm('form')">
+    <md-field :class="getValidationClass('form','id')">
       <label>Nombre</label>
-      <md-input v-model="newAgreement.id" md-counter="30"></md-input>
+      <md-input v-model="form.id" md-counter="30"></md-input>
+      <span class="md-error" v-if="!$v.form.id.required">Se requiere nombre para el acuerdo</span>
+      <span class="md-error" v-else-if="!$v.form.id.maxlength">Exceso de carácteres</span>
     </md-field>
 
     <md-field>
       <label>Usuario</label>
-      <md-input v-model="newAgreement.user.name" md-counter="30"></md-input>
+      <md-input v-model="form.user.name" md-counter="30"></md-input>
     </md-field>
 
     <md-field>
       <label>Rider</label>
-      <md-input v-model="newAgreement.rider.name" md-counter="30"></md-input>
+      <md-input v-model="form.rider.name" md-counter="30"></md-input>
     </md-field>
 
-    <md-field>
+    <md-field :class="getValidationClass('form','junk')">
       <label>Residuo</label>
-      <md-input v-model="newAgreement.junk.name" md-counter="30"></md-input>
+      <md-input v-model="form.junk.name" md-counter="30"></md-input>
+      <span class="md-error" v-if="!$v.form.junk.required">Se requiere nombre del residuo</span>
+      <span class="md-error" v-else-if="!$v.form.junk.maxlength">Exceso de carácteres</span>
     </md-field>
 
-    <md-field>
+    <md-field :class="getValidationClass('form','date')">
       <label>Fecha</label>
-      <md-input v-model="newAgreement.date" md-counter="30"></md-input>
+      <md-input v-model="form.date" md-counter="10"></md-input>
+      <span class="md-error" v-if="!$v.form.date.required">Se requiere fecha</span>
+      <span class="md-error" v-else-if="!$v.form.date.maxlength">Exceso de carácteres</span>
     </md-field>
 
-    <md-field :md-counter="false">
+    <md-field :class="getValidationClass('form','price')" :md-counter="false">
       <label>Precio (€)</label>
-      <md-input v-model="newAgreement.price" maxlength="10"></md-input>
+      <md-input v-model="form.price" maxlength="10"></md-input>
+      <span class="md-error" v-if="!$v.form.price.required">Se requiere establecer un precio</span>
+      <span class="md-error" v-else-if="!$v.form.price.decimal">Se requiere un número decimal</span>
     </md-field>
 
-    <md-field>
+    <md-field :class="getValidationClass('form','location')">
       <label>Ubicación</label>
-      <md-input v-model="newAgreement.location" maxlength="30"></md-input>
+      <md-input v-model="form.location" maxlength="50"></md-input>
+      <span class="md-error" v-if="!$v.form.location.required">Se requiere poner una localización</span>
+      <span class="md-error" v-else-if="!$v.form.location.maxlength">Exceso de carácteres</span>
     </md-field>
 
 
-    <md-button class="md-raised md-primary" @click="addAgreement">Aceptar</md-button>
+    <md-button type="submit" class="md-raised md-primary" @click="addAgreement">Aceptar</md-button>
     <md-button to ='/agreements' class="md-raised md-accent">Cancelar</md-button>
 
     
-    
+</form>
   </div>
 </template>
 
@@ -49,66 +60,105 @@
 
 
 <script>
+import { mapMutations } from "vuex";
+import { validationMixin } from "vuelidate";
+import { required, maxLength, decimal } from "vuelidate/lib/validators";
 
-import { mapMutations} from 'vuex';
 
 export default {
   name: "Counters",
+  mixins: [validationMixin],
   data: () => ({
-    newAgreement:{
-    id: '',
-    user: {
-      name: ''
+    form: {
+      id: "",
+      user: {
+        name: ""
       },
-    rider:{
-      name: ''
+      rider: {
+        name: ""
+      },
+      junk: {
+        name: ""
+      },
+      date: "",
+      location: "",
+      price: ""
     },
-    junk:{
-      name: ''
-    },
-    date: '',
-    location: '',
-    price: '',
-    },
-    textarea: '',
-    deadline: ''
+    textarea: "",
+    deadline: ""
   }),
+  validations: {
+    form: {
+      id: {
+        required,
+        maxLength: maxLength(30)
+      },
+      junk: {
+        required,
+        maxLength: maxLength(30)
+      },
+      date: {
+        required,
+        maxLength: maxLength(10)
+      },
+      price: {
+        required,
+        decimal
+      },
+      location: {
+        required,
+        maxLength: maxLength(40)
+      }
+    }
+  },
   methods: {
-   ...mapMutations([
-     'ADD_AGREEMENT'
-   ]),
-   addAgreement: function(){
-        if ( this.newAgreement.id == '' ||
-            this.newAgreement.user == '' ||
-            this.newAgreement.rider.name == '' ||
-            this.newAgreement.user.name == '' ||
-            this.newAgreement.junk.name == '' ||
-            this.newAgreement.date == '' ||
-            this.newAgreement.location == '' ||
-            this.newAgreement.price == ''
-        ) {
-          alert("Campos vacíos");
-          return;
-        }
-        this.ADD_AGREEMENT(this.newAgreement);
-        this.$router.push('agreements')
-   },
-   clearForm: function(){
-        this.newAgreement.id= ''
-        this.newAgreement.user= ''
-        this.newAgreement.rider.name= ''
-        this.newAgreement.user.name= ''
-        this.newAgreement.junk.name= ''
-        this.newAgreement.date= ''
-        this.newAgreement.location= ''
-        this.newAgreement.price= ''
-   }
+    ...mapMutations(["ADD_AGREEMENT"]),
+    addAgreement: function() {
+      if (
+        this.form.id == "" ||
+        this.form.user == "" ||
+        this.form.rider.name == "" ||
+        this.form.user.name == "" ||
+        this.form.junk.name == "" ||
+        this.form.date == "" ||
+        this.form.location == "" ||
+        this.form.price == ""
+      ) {
+        return;
+      }
+      this.ADD_AGREEMENT(this.form);
+      this.$router.push("agreements");
+    },
+    clearForm: function() {
+      this.form.id = "";
+      this.form.user = "";
+      this.form.rider.name = "";
+      this.form.user.name = "";
+      this.form.junk.name = "";
+      this.form.date = "";
+      this.form.location = "";
+      this.form.price = "";
+    },
+     getValidationClass(form, fieldName) {
+      const field = this.$v[form][fieldName];
+      if (field) {
+        return {
+          "md-invalid": field.$invalid && field.$dirty
+        };
+      }
+    },
+    validateForm(form, callback) {
+      this.$v.$touch();
+      if (!this.$v[form].$invalid) {
+        callback();
+      }
+    }
   }
 };
 </script>
 
 <style>
-#form {
+#formulario {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
